eab4b68edc0a97fbfd96f238b89e1bc574031362
Javier Augusto Alba <javieraugustoalba@gmail.com>
2024-02-24 01:08:02 -0500

    add app and package.json file

--- ToolConFE/src/App.js
+++ ToolConFE/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login/LoginPage'; // Adjust the import path as necessary
+import Dashboard from './components/Dashboard/Dashboard'; // Adjust the import path as necessary
+import WorkersManagement from './components/WorkersManagement/WorkersManagement';
+import ToolManagement from './components/ToolManagement/ToolManagement';
+import Reports from './components/Reports/Reports';
+import CreateTool from './components/ToolManagement/CreateTool';
+import LeftNav from './components/LeftNav/LeftNav';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tool-management" element={<ToolManagement />} />
+        <Route path="/leftnav" element={<LeftNav />} />
+        <Route path="/workers-management" element={<WorkersManagement />} />
+        <Route path="/create-tool" element={<CreateTool />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+    </Router>
   );
 }
 
